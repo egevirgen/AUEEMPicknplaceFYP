1d9f6fd44855c274297ccedd93c0a87f63ce26b9
egevirgen <egevirgen@gmail.com>
2017-10-09 23:25:06 +0300

    Center accuracy artmasina yarayan commit
    boundingrect komutu ile cevreleyen en kucuk dikdortgen ciziliyor. Bunun baslangic koodinatlarini, yukseklik ve genisligini elde edebiliyoruz. Ornegin sagdan sola dogru akacak olan bir bant icin cismin tam olarak kareye girdiginden emin olduktan sonra center verisi gondermeliyiz ki hata payi azalsin. Bu commit onu saglayacak

--- Python/WebcamDetection.py
+++ Python/WebcamDetection.py
@@ -6,7 +6,7 @@ import numpy as np
 import imutils
 cap = cv2.VideoCapture(0)    
 while(1):
-  try:
+ 
   	ret, frame = cap.read()
  	gray_vid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  	denoised = cv2.GaussianBlur(gray_vid,(5,5),0)
@@ -16,8 +16,6 @@ while(1):
   	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE) 
   	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-        
 	for c in cnts:
 	# compute the center of the contour
 		M = cv2.moments(c)
@@ -25,21 +23,29 @@ while(1):
 			M["m00"]=1
 		cX = int(M["m10"] / M["m00"])
 		cY = int(M["m01"] / M["m00"])
-		print(M["m10"])
-	
+
+		area = cv2.contourArea(c)
+		x,y,w,h = cv2.boundingRect(c)
+
+		cv2.circle(frame, (x+w, y+h), 7, (255, 255, 255), -1)	
+		cv2.circle(frame, (x, y), 7, (255, 255, 255), -1)
+
+		if x > 10 and x < 650 and x+w < 650 and y > 10 and y < 450 and y+h < 450 :
+			print x,y,w,h
+			print "---"
+		
 	# draw the contour and center of the shape on the image
-		cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-		cv2.circle(frame, (cX, cY), 7, (255, 255, 0), -1)
+		cv2.drawContours(frame, [c], -1, (255, 255, 0), 2)
+		cv2.circle(frame, (cX, cY), 7, (255, 255, 0), -1)	
 		cv2.putText(frame, "center", (cX - 20, cY - 20),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
- 
+	
 # show the output image
   	cv2.imshow("Frame", frame)
-	cv2.imshow("thresh",thresh)
-  except:
-	pass
 
-  if cv2.waitKey(10)==ord('q'):
-        break
+
+
+  	if cv2.waitKey(10)==ord('q'):
+        	break
 cap.release()
 cv2.destroyAllWindows()
