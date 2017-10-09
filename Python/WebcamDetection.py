71db12d8e9edce9795ec6be0924e231f7c4b350c
bariskesen96 <32616521+bariskesen96@users.noreply.github.com>
2017-10-09 18:20:36 +0300

    Update WebcamDetection.py
    moments detection kullanılmaya başlandı. cnts ve moments değerlerinin hangi durumda ne döndürdüğünü öğrenip  ona göre merkez bulma algoritması geliştirilecek

--- Python/WebcamDetection.py
+++ Python/WebcamDetection.py
@@ -10,34 +10,36 @@ while(1):
   	ret, frame = cap.read()
  	gray_vid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  	denoised = cv2.GaussianBlur(gray_vid,(5,5),0)
- 	thresh = cv2.threshold(denoised, 45, 255, cv2.THRESH_BINARY)[1]
+ 	thresh = cv2.threshold(denoised, 50, 255, cv2.THRESH_BINARY)[1] 
  	thresh = cv2.erode(thresh, None, iterations=2)
  	thresh = cv2.dilate(thresh, None, iterations=2)
   	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_NONE)
+	cv2.CHAIN_APPROX_SIMPLE) 
   	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
         
 	for c in cnts:
-		c = max(cnts, key=cv2.contourArea)
- 		extLeft = tuple(c[c[:, :, 0].argmin()][0])
-  		extRight = tuple(c[c[:, :, 0].argmax()][0])
-  		extTop = tuple(c[c[:, :, 1].argmin()][0])
-  		extBot = tuple(c[c[:, :, 1].argmax()][0])
-  		cv2.drawContours(frame, [c], -1, (0, 255, 255), 2)
-  		cv2.circle(frame, extLeft, 8, (0, 0, 255), -1)
-  		cv2.circle(frame, extRight, 8, (0, 255, 0), -1)
-  		cv2.circle(frame, extTop, 8, (255, 0, 0), -1)
-  		cv2.circle(frame, extBot, 8, (255, 255, 0), -1)
-  		extCenter = ((extRight[0]+extLeft[0])/2,(extBot[1]+extTop[1])/2)
-  		cv2.putText(frame, "center_4kenar", (extCenter[0]-20,extCenter[1]-20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
- 		cv2.circle(frame, extCenter, 8, (255, 255, 0), -1)
+	# compute the center of the contour
+		M = cv2.moments(c)
+		if (M["m00"] == 0):
+			M["m00"]=1
+		cX = int(M["m10"] / M["m00"])
+		cY = int(M["m01"] / M["m00"])
+		print(M["m10"])
+	
+	# draw the contour and center of the shape on the image
+		cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+		cv2.circle(frame, (cX, cY), 7, (255, 255, 0), -1)
+		cv2.putText(frame, "center", (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+ 
 # show the output image
   	cv2.imshow("Frame", frame)
+	cv2.imshow("thresh",thresh)
   except:
 	pass
 
   if cv2.waitKey(10)==ord('q'):
         break
-cam.release()
+cap.release()
 cv2.destroyAllWindows()
