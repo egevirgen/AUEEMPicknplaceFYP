6963f2a8dedcf93c7184736a22cf36bbb4b6692f
bariskesen96 <32616521+bariskesen96@users.noreply.github.com>
2017-10-19 19:38:28 +0300

    Update WebcamDetection.py

--- Python/WebcamDetection.py
+++ Python/WebcamDetection.py
@@ -18,7 +18,7 @@ center_koordinatlari_x=[]
 center_koordinatlari_y=[]
 
 #Serial init
-#ser = serial.Serial("/dev/ttyUSB0", baudrate = 115200, timeout = 1)
+ser = serial.Serial("/dev/ttyUSB0", baudrate = 115200, timeout = 1)
 
 
 # Image process def
@@ -28,7 +28,7 @@ def image_process(frame):
 	#Blur
  	denoised = cv2.medianBlur(gray_vid,5)
 	#Threshold
- 	thresh = cv2.threshold(denoised, 50, 255, cv2.THRESH_BINARY)[1] 
+ 	thresh = cv2.threshold(denoised, 50, 255, cv2.THRESH_BINARY)[1]
  	thresh = cv2.erode(thresh, None, iterations=2)
  	thresh = cv2.dilate(thresh, None, iterations=2)
 
@@ -43,7 +43,7 @@ def find_contours(frame_thresh):
 
 	#Contour detection
         cnts = cv2.findContours(frame_thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE) 
+	cv2.CHAIN_APPROX_SIMPLE)
   	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
 	for c in cnts:
@@ -61,24 +61,24 @@ def find_contours(frame_thresh):
 
 			# Draw the contour and center of the shape on the image
 			cv2.drawContours(frame, [c], -1, (255, 255, 0), 2)
-			cv2.circle(frame, (cX, cY), 7, (255, 255, 0), -1)	
+			cv2.circle(frame, (cX, cY), 7, (255, 255, 0), -1)
 			cv2.putText(frame, "center", (cX - 20, cY - 20),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
 
-			# Sekil okunabilir halde ise center noktasini al			
+			# Sekil okunabilir halde ise center noktasini al
 			if x+w<630 and x>10 and y>10 and y+h<350 :
 				center_sayisi=center_sayisi+1
 				center_koordinatlari_x.append(x)
 				center_koordinatlari_y.append(y)
-			
+
 	return center_sayisi,center_koordinatlari_x,center_koordinatlari_y;
 
 #Karar mekanizmasi
 def karar(center_temp_x,center_temp_y) :
-	index, value = max(enumerate(center_temp_x), key=operator.itemgetter(1))				
+	index, value = max(enumerate(center_temp_x), key=operator.itemgetter(1))
  	return center_temp_x[index],center_temp_y[index]
 
-#Veri gönderme
+#Veri gonderme
 def uart(center_temp,center_memory,center_temp_x,center_temp_y) :
 	if center_temp > center_memory :
 		l = []
@@ -98,11 +98,13 @@ def uart(center_temp,center_memory,center_temp_x,center_temp_y) :
 		x+='k'
 		return x
 while(1):
- 
+
         # Capture webcam frame
   	ret, frame = cap.read()
 	center_temp,center_temp_x,center_temp_y = find_contours(image_process(frame))
-	#ser.write(uart.encode('utf-8'))
+	if uart(center_temp,center_memory,center_temp_x,center_temp_y) != None:
+		print uart(center_temp,center_memory,center_temp_x,center_temp_y)
+		ser.write(uart(center_temp,center_memory,center_temp_x,center_temp_y).encode('utf-8'))
 	center_memory=center_temp
 	# Show the output image
   	cv2.imshow("Frame", frame)
@@ -111,8 +113,6 @@ while(1):
 	if cv2.waitKey(10)==ord('q'):
         	break
 
- 
+
 cap.release()
 cv2.destroyAllWindows()
-
-  
